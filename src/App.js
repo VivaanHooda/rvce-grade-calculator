acdabcb4077b65146426cde821b7c8814280838b
VivaanHooda <vivaan.hooda@gmail.com>
2025-09-21 02:35:23 +0530

    Bug Fixes

--- src/App.js
+++ src/App.js
@@ -595,7 +595,7 @@ const SubjectForm = ({ subject, formData, currentMode, onCalculate, subjectGrade
             value={inputValues.see}
             onChange={(e) => handleInputChange('see', e.target.value)}
             className="w-full px-3 sm:px-4 py-2 sm:py-3 border border-gray-300 rounded-xl focus:ring-2 focus:ring-blue-500 focus:border-transparent transition-all text-center text-sm sm:text-lg font-medium"
-            placeholder="0"
+            placeholder=""
             ref={refs.see}
             onKeyDown={(e) => handleKeyDown(e, 'see')}
           />
