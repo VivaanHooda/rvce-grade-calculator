a19c97993fffc05c2eda4092f89df9ec2866b46c
VivaanHooda <vivaan.hooda@gmail.com>
2026-01-24 23:19:45 +0530

    UI fix

--- src/App.jsx
+++ src/App.jsx
@@ -128,7 +128,7 @@ const FinalCGPAView = React.memo(({
           onClick={() => handleSetCurrentMode('')}
           className="text-blue-600 hover:text-blue-700 font-medium text-lg transition-colors"
         >
-          ← Back to Calculation Mode
+          ← Back
         </button>
         <div className="w-32"></div>
       </div>
